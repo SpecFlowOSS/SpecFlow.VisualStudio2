e35aa377c57a4fbc250599ed541256b321dc52e1
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-01-21 17:56:55 +0100

    add empty classifier project

---
+++ SpecFlow.VisualStudio.Editor/EditorClassifierType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SpecFlow.VisualStudio.Editor
+{
+    internal static class EditorClassifierClassificationDefinition
+    {
+        /// <summary>
+        /// Defines the "EditorClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("EditorClassifier")]
+        internal static ClassificationTypeDefinition EditorClassifierType = null;
+    }
+}
