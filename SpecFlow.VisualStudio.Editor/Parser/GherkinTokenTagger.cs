ca8e535ed60122485981f2ec803aa3b007ef0485
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-03-04 12:01:47 +0100

    cleanup token tagger

--- SpecFlow.VisualStudio.Editor/Parser/GherkinTokenTagger.cs
+++ SpecFlow.VisualStudio.Editor/Parser/GherkinTokenTagger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Gherkin;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Tagging;
@@ -11,10 +12,21 @@ namespace SpecFlow.VisualStudio.Editor.Parser
 {
     internal sealed class GherkinTokenTagger : ITagger<GherkinTokenTag>
     {
+        private class ParsingResult
+        {
+            public int SnapshotVersion { get; private set; }
+            public GherkinTokenTag[] Tags { get; private set; }
+
+            public ParsingResult(int snapshotVersion, GherkinTokenTag[] tags)
+            {
+                SnapshotVersion = snapshotVersion;
+                Tags = tags;
+            }
+        }
+
         private readonly ITextBuffer buffer;
 
-        private GherkinTokenTag[] lastParsedTokenTags;
-        private int lastParsedBufferVersion = -1;
+        private ParsingResult lastResult = null;
 
         internal GherkinTokenTagger(ITextBuffer buffer)
         {
@@ -47,11 +59,14 @@ namespace SpecFlow.VisualStudio.Editor.Parser
 
         private GherkinTokenTag[] Parse(ITextSnapshot snapshot)
         {
-            if (snapshot.Version.VersionNumber == lastParsedBufferVersion)
-                return lastParsedTokenTags;
-
+            var currentLastResult = lastResult;
+            if (currentLastResult != null && currentLastResult.SnapshotVersion == snapshot.Version.VersionNumber)
+                return currentLastResult.Tags;
+                
             var fileContent = snapshot.GetText();
 
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             var parserErrors = new List<ParserException>();
 
             var parser = new GherkinEditorParser();
@@ -82,15 +97,18 @@ namespace SpecFlow.VisualStudio.Editor.Parser
             tokenTags.AddRange(parserErrors.Select(e => new GherkinTokenTag(e, snapshot)));
             tokenTags.Sort((t1, t2) => t1.Span.Start.Position.CompareTo(t2.Span.Start.Position));
 
-            //TODO: atomic set?
-            lastParsedBufferVersion = snapshot.Version.VersionNumber;
-            lastParsedTokenTags = tokenTags.ToArray();
+            stopwatch.Stop();
+            Debug.WriteLine("Gherkin3: parsed v{0} on thread {1} in {2} ms", snapshot.Version.VersionNumber, Thread.CurrentThread.ManagedThreadId, stopwatch.ElapsedMilliseconds);
+
+            var result = new ParsingResult(snapshot.Version.VersionNumber, tokenTags.ToArray());
+            Thread.MemoryBarrier();
+            lastResult = result;
 
             //TODO: only for the changed scope
             if (TagsChanged != null)
                 TagsChanged(this, new SnapshotSpanEventArgs(new SnapshotSpan(snapshot, 0, snapshot.Length)));
 
-            return lastParsedTokenTags;
+            return result.Tags;
         }
     }
 }
